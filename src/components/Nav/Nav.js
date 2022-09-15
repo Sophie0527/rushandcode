0c8b498c7e2389904eca913eaee7748ef7712188
손소희 <ssh30510044@gmail.com>
2022-09-15 09:46:22 +0900

    Fix: 불필요한 코드 삭제

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-// import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
 import logo from '../Nav/image/rush_logo.png';
 import styled from 'styled-components';
