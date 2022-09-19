12dfa3f7a2d7d9479ebddab125c3a0f7550eb639
손소희 <ssh30510044@gmail.com>
2022-09-20 01:01:36 +0900

    Add: 로그인페이지 ui 구현

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import styled from 'styled-components';
-import { CustomMediaStyle } from '../../styles/CustomMediaStyle';
 
 function Login() {
+  const navigate = useNavigate();
   return (
     <Container>
       <Header>
@@ -13,9 +14,43 @@ function Login() {
         </h3>
       </Header>
       <ContentsBox>
-        <div>아이디</div>
-        <div>비번</div>
+        <InputBox>
+          <img
+            src="https://img.icons8.com/external-tanah-basah-glyph-tanah-basah/344/external-user-user-tanah-basah-glyph-tanah-basah-7.png"
+            alt="id"
+          />
+          <input type="text" placeholder="아이디"></input>
+        </InputBox>
+        <InputBox>
+          <img
+            src="https://img.icons8.com/material-rounded/344/lock--v1.png"
+            alt="pw"
+          />
+          <input type="password" placeholder="비밀번호"></input>
+        </InputBox>
       </ContentsBox>
+      <SaveId>
+        <div>
+          <input type="checkbox"></input>
+          <span>아이디 저장</span>
+        </div>
+      </SaveId>
+      <LoginBox>
+        <button>로그인</button>
+      </LoginBox>
+      <SignupBox>
+        <span
+          onClick={() => {
+            navigate('/signup');
+          }}
+        >
+          회원가입
+        </span>
+        <p>|</p>
+        <span>아이디 찾기</span>
+        <p>|</p>
+        <span>비밀번호 찾기</span>
+      </SignupBox>
     </Container>
   );
 }
@@ -23,35 +58,34 @@ function Login() {
 const Container = styled.div`
   width: 100%;
   height: 100%;
-  padding-top: 125px;
+  padding-top: 140px;
 `;
 const Header = styled.div`
-  padding-top: 70px;
+  padding-top: 100px;
   display: flex;
   flex-direction: column;
   text-align: center;
-  ${CustomMediaStyle.lessThan('mobile')`
-  padding-top: 50px;
-  `}
   h1 {
     font-size: 45px;
     font-weight: 700;
+    letter-spacing: 2px;
   }
   h3 {
-    padding: 40px 0;
-    display: flex;
-    justify-content: center;
+    padding: 60px 0;
     font-size: 18px;
     font-weight: 600;
-
+    display: flex;
+    justify-content: center;
     span {
-      padding: 0 10px;
-      color: #9e9e9e;
       letter-spacing: 1.5px;
+      padding: 0 80px;
+      cursor: pointer;
     }
     p {
-      padding: 0 10px;
+      color: #9e9e9e;
       letter-spacing: 1.5px;
+      padding: 0 80px;
+      cursor: pointer;
     }
   }
 `;
@@ -60,6 +94,87 @@ const ContentsBox = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+const InputBox = styled.div`
+  border: 1px solid black;
+  border-radius: 2px;
+  display: flex;
+  align-items: center;
+  margin: 5px 0;
+  img {
+    width: 22px;
+    padding-left: 10px;
+  }
+  input {
+    text-align: center;
+    border: none;
+    width: 400px;
+    height: 45px;
+    outline: none;
+    margin-right: 30px;
+    font-size: 15px;
+    letter-spacing: 1px;
+    ::placeholder {
+      text-align: center;
+      letter-spacing: 1px;
+    }
+  }
+`;
+
+const SaveId = styled.div`
+  display: flex;
+  justify-content: center;
+  div {
+    width: 470px;
+    padding: 15px 0;
+    display: flex;
+    align-items: center;
+    input {
+      width: 15px;
+      height: 15px;
+      cursor: pointer;
+    }
+    span {
+      font-size: 15px;
+      padding-left: 5px;
+    }
+  }
+`;
+
+const LoginBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 20px;
+  button {
+    width: 470px;
+    height: 50px;
+    color: white;
+    font-size: 17px;
+    font-weight: 600;
+    letter-spacing: 1px;
+    background-color: black;
+    border: none;
+    border-radius: 2px;
+    cursor: pointer;
+  }
+`;
+
+const SignupBox = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 40px 0 160px;
+  font-weight: 500;
+  color: #9e9e9e;
+  span:hover {
+    color: black;
+  }
+  span {
+    padding: 0 40px;
+    cursor: pointer;
+  }
 `;
 
 export default Login;
