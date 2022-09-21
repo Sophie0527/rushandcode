fcd2cd1ab40125265997529fcc8741a2488a367c
손소희 <ssh30510044@gmail.com>
2022-09-21 13:58:46 +0900

    Add: products 페이지 반응형 구현

--- src/pages/Products/Products.js
+++ src/pages/Products/Products.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import { CustomMediaStyle } from '../../styles/CustomMediaStyle';
 
 function Products() {
   const [products, setProducts] = useState([]);
@@ -102,6 +103,12 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
   padding-top: 125px;
+  ${CustomMediaStyle.lessThan('tablet')`
+  padding-top: 120px;
+  `}
+  ${CustomMediaStyle.lessThan('mobile')`
+  padding-top: 110px;
+  `}
 `;
 
 const TopBannerBox = styled.div`
@@ -109,6 +116,12 @@ const TopBannerBox = styled.div`
     width: 100%;
     height: 500px;
     object-fit: cover;
+    ${CustomMediaStyle.lessThan('tablet')`
+    height: 400px;
+    `}
+    ${CustomMediaStyle.lessThan('mobile')`
+    height: 300px;
+    `}
   }
   div {
     background-color: rgba(0, 0, 0, 0.4);
@@ -120,17 +133,37 @@ const TopBannerBox = styled.div`
     flex-direction: column;
     justify-content: center;
     text-align: center;
+    ${CustomMediaStyle.lessThan('tablet')`
+    height: 400px;
+    `}
+    ${CustomMediaStyle.lessThan('mobile')`
+    height: 300px;
+    `}
     h1 {
       font-family: 'Nanum Brush Script', cursive;
       font-style: italic;
       font-size: 130px;
       font-weight: 700;
       padding: 5px 0;
+      ${CustomMediaStyle.lessThan('tablet')`
+      font-size: 110px;
+      font-weight: 700;
+      `}
+      ${CustomMediaStyle.lessThan('mobile')`
+      font-size: 100px;
+      font-weight: 700;
+      `}
     }
     h3 {
       font-size: 30px;
       padding: 5px 0;
       letter-spacing: 1.5px;
+      ${CustomMediaStyle.lessThan('tablet')`
+      font-size: 22px;
+      `}
+      ${CustomMediaStyle.lessThan('mobile')`
+      font-size: 18px;
+      `}
     }
   }
 `;
@@ -156,6 +189,10 @@ const CategoryBox = styled.div`
     color: #949494;
     padding-right: 20px;
     cursor: pointer;
+    ${CustomMediaStyle.lessThan('mobile')`
+    font-size: 17px;
+    padding-right: 13px;
+   `}
   }
 `;
 
@@ -168,10 +205,18 @@ const FilterBox = styled.div`
   align-items: center;
   padding: 0 20px;
   cursor: pointer;
+  ${CustomMediaStyle.lessThan('mobile')`
+  height: 40px;
+  padding: 0 10px;
+  `}
   span {
     width: 100px;
     font-size: 18px;
     letter-spacing: 1px;
+    ${CustomMediaStyle.lessThan('mobile')`
+    width: 85px;
+    font-size: 16px;
+    `}
   }
   img {
     width: 15px;
@@ -186,6 +231,10 @@ const Filter = styled.div`
   flex-direction: column;
   padding: 10px 0;
   position: absolute;
+  ${CustomMediaStyle.lessThan('mobile')`
+  width: 120px;
+  padding: 5px 0;
+  `}
   span:hover {
     background-color: #eaeaea;
   }
@@ -194,6 +243,11 @@ const Filter = styled.div`
     padding: 15px 20px;
     font-size: 18px;
     letter-spacing: 1px;
+    ${CustomMediaStyle.lessThan('mobile')`
+    padding: 10px 10px;
+    font-size: 16px;
+    letter-spacing: 0.5px;
+   `}
   }
 `;
 
@@ -207,6 +261,9 @@ const ProductWrap = styled.div`
   flex-wrap: wrap;
   justify-content: center;
   padding-bottom: 100px;
+  ${CustomMediaStyle.lessThan('desktop')`
+  width: 95%;
+  `}
   div:hover {
     background-color: #eaeaea;
   }
@@ -222,27 +279,47 @@ const ProductBox = styled.div`
   padding: 20px 0;
   margin: 15px;
   cursor: pointer;
+  ${CustomMediaStyle.lessThan('desktop')`
+  width: 260px;
+  `}
+  ${CustomMediaStyle.lessThan('tablet')`
+  width: 190px;
+  padding: 0px;
+  `}
   h2 {
     padding: 8px 0;
     font-size: 28px;
     font-weight: 600;
+    ${CustomMediaStyle.lessThan('tablet')`
+    font-size: 25px;
+    `}
   }
   h3 {
     padding: 8px 0;
     font-size: 20px;
     font-weight: 500;
     color: #535353;
+    ${CustomMediaStyle.lessThan('tablet')`
+    padding: 3px 0;
+    font-size: 18px;
+    `}
   }
   span {
     padding: 8px 0 20px;
     font-size: 18px;
     color: #535353;
+    ${CustomMediaStyle.lessThan('tablet')`
+    font-size: 16px;
+    `}
   }
 `;
 
 const ImgBox = styled.div`
   img {
     width: 250px;
+    ${CustomMediaStyle.lessThan('desktop')`
+    width: 200px;
+    `}
   }
 `;
 
@@ -252,6 +329,9 @@ const VeganBox = styled.div`
   align-items: center;
   height: 25px;
   padding-bottom: 10px;
+  ${CustomMediaStyle.lessThan('tablet')`
+  height: 20px;
+  `}
   p {
     border: 2px solid green;
     border-radius: 50px;
@@ -259,6 +339,9 @@ const VeganBox = styled.div`
     color: green;
     font-size: 15px;
     font-weight: 700;
+    ${CustomMediaStyle.lessThan('tablet')`
+    font-size: 14px;
+    `}
   }
 `;
 
