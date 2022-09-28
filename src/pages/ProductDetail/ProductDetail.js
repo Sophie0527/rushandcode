9158541d0a8474695d8c72dfc75f8d8ac8dfe5f0
손소희 <ssh30510044@gmail.com>
2022-09-28 20:30:10 +0900

    Add: 상품디테일 페이지 데이터 불러와서 보여주기

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -1,68 +1,131 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import styled from 'styled-components';
 import { AiOutlineHeart } from 'react-icons/ai';
-// import { AiFillHeart } from 'react-icons/ai';
+import { AiFillHeart } from 'react-icons/ai';
 import { AiOutlineShopping } from 'react-icons/ai';
 import { CustomMediaStyle } from '../../styles/CustomMediaStyle';
 
 function ProductDetail() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('http://localhost:3000/data/ProductData/Product.json', {
+      method: 'GET',
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, [setProducts]);
+
+  const pathname = useLocation().pathname;
+  const idNum = Number(pathname.split('/')[2]);
+
+  const productInfo = products.filter((products) => products.id === idNum);
+
+  const [like, setLike] = useState(false);
+
   return (
     <Container>
-      <LeftBox>
-        <img
-          src="https://cdn.pixabay.com/photo/2021/01/24/10/01/bath-5944728_1280.png"
-          alt="예시"
-        ></img>
-        <SubImageBox>
-          <img
-            src="https://cdn.pixabay.com/photo/2021/01/24/10/01/bath-5944728_1280.png"
-            alt="예시"
-          ></img>
-          <img
-            src="https://cdn.pixabay.com/photo/2021/01/24/10/01/bath-5944728_1280.png"
-            alt="예시"
-          ></img>
-          <img
-            src="https://cdn.pixabay.com/photo/2021/01/24/10/01/bath-5944728_1280.png"
-            alt="예시"
-          ></img>
-        </SubImageBox>
-      </LeftBox>
-      <RightBox>
-        <ProductInfoBox>
-          <GotoReview>
-            <span>후기 보기</span>
-            <img
-              src="https://img.icons8.com/material-rounded/344/share.png"
-              alt="공유"
-            ></img>
-          </GotoReview>
-          <ProductInfo>
-            <img
-              src="https://cdn.pixabay.com/photo/2021/01/24/10/01/bath-5944728_1280.png"
-              alt="예시"
-            ></img>
-            <h1>인터갈락틱</h1>
-            <h3>배쓰 밤</h3>
-            <p>#배스밤 #환상적인우주느낌</p>
-          </ProductInfo>
-          <TotalPrice>
-            <span>총 합계 금액</span>
-            <h1>₩ 17000</h1>
-          </TotalPrice>
-          <BuyBox>
-            <ButtonBox>
-              <AiOutlineHeart />
-            </ButtonBox>
-            <ButtonBox>
-              <AiOutlineShopping />
-            </ButtonBox>
-            <BuyButtonBox>
-              <span>바로구매</span>
-            </BuyButtonBox>
-          </BuyBox>
-        </ProductInfoBox>
-      </RightBox>
+      {productInfo.map((product, idx) => {
+        return (
+          <div key={idx}>
+            <BoxWrap>
+              <LeftBox>
+                <img src={product.sub_img} alt={product.product_sub_name}></img>
+                <SubImageBox>
+                  <img
+                    src={product.sub_img}
+                    alt={product.product_sub_name}
+                  ></img>
+                  <img src={product.img} alt={product.product_sub_name}></img>
+                </SubImageBox>
+              </LeftBox>
+              <RightBox>
+                <ProductInfoBox>
+                  <GotoReview>
+                    <div>
+                      <span>{product.reviews.length}개의 후기 보기</span>
+                    </div>
+                    <img
+                      src="https://img.icons8.com/material-rounded/344/share.png"
+                      alt="공유"
+                    ></img>
+                  </GotoReview>
+                  <ProductInfo>
+                    <img src={product.img} alt={product.product_sub_name}></img>
+                    <h1>{product.product_name}</h1>
+                    <h3>{product.product_sub_name}</h3>
+                    <p>{product.hashtag}</p>
+                  </ProductInfo>
+                  <TotalPrice>
+                    <span>총 합계 금액</span>
+                    <h1>
+                      ₩&nbsp;
+                      {product.price
+                        .toString()
+                        .replace(/\B(?=(\d{3})+(?!\d))/g, ',')}
+                    </h1>
+                  </TotalPrice>
+                  <BuyBox>
+                    {!like ? (
+                      <ButtonBox onClick={() => setLike(true)}>
+                        <AiOutlineHeart />
+                      </ButtonBox>
+                    ) : (
+                      <ButtonBox onClick={() => setLike(false)}>
+                        <AiFillHeart />
+                      </ButtonBox>
+                    )}
+                    <ButtonBox>
+                      <AiOutlineShopping />
+                    </ButtonBox>
+                    <BuyButtonBox>
+                      <span>바로구매</span>
+                    </BuyButtonBox>
+                  </BuyBox>
+                </ProductInfoBox>
+              </RightBox>
+            </BoxWrap>
+
+            <FilterBox>
+              <div>
+                <span>제품정보</span>
+              </div>
+              <div>
+                <span>제품후기</span>
+              </div>
+              <div>
+                <span>배송/반품/교환 안내</span>
+              </div>
+              <div>
+                <span>상품필수정보</span>
+              </div>
+            </FilterBox>
+
+            <ReviewBox>
+              <h1>총&nbsp;{product.reviews.length}개의 후기</h1>
+              {product.reviews.map((review, idx) => {
+                return (
+                  <ReviewContent key={idx}>
+                    <ReviewLeftBox>
+                      <div>
+                        <span>{review.star}</span>
+                        <p>{review.user_name.replace(/(?<=.{1})./, '*')}</p>
+                      </div>
+                      <ReviewText>{review.content}</ReviewText>
+                    </ReviewLeftBox>
+                    <ReviewRightBox>
+                      <p>{review.date}</p>
+                    </ReviewRightBox>
+                  </ReviewContent>
+                );
+              })}
+            </ReviewBox>
+          </div>
+        );
+      })}
     </Container>
   );
 }
@@ -70,14 +133,17 @@ const Container = styled.div`
   width: 100%;
   height: 100%;
   padding-top: 125px;
-  display: flex;
-  justify-content: center;
   ${CustomMediaStyle.lessThan('tablet')`
   padding-top: 120px;
   `}
   ${CustomMediaStyle.lessThan('mobile')`
   padding-top: 110px;
   `}
+`;
+
+const BoxWrap = styled.div`
+  display: flex;
+  justify-content: center;
 `;
 
 const LeftBox = styled.div`
@@ -111,12 +177,17 @@ const GotoReview = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  span {
-    font-size: 18px;
-    font-weight: 500;
-    color: #535353;
-    cursor: pointer;
+  div {
+    border-bottom: 1.5px solid #535353;
+    padding-bottom: 2px;
+    span {
+      font-size: 18px;
+      font-weight: 500;
+      color: #535353;
+      cursor: pointer;
+    }
   }
+
   img {
     width: 25px;
     cursor: pointer;
@@ -127,7 +198,7 @@ const ProductInfo = styled.div`
   text-align: center;
   padding: 30px 60px;
   img {
-    width: 100%;
+    width: 300px;
     padding: 5px 0;
   }
   h1 {
@@ -141,7 +212,9 @@ const ProductInfo = styled.div`
     padding: 10px 0;
   }
   p {
+    font-size: 18px;
     padding: 10px 0;
+    color: #535353;
   }
 `;
 
@@ -151,8 +224,8 @@ const TotalPrice = styled.div`
   align-items: center;
   padding-top: 20px;
   span {
-    font-size: 18px;
-    font-weight: 500;
+    font-size: 19px;
+    font-weight: 400;
     color: #535353;
     cursor: pointer;
   }
@@ -166,7 +239,7 @@ const BuyBox = styled.div`
   display: flex;
   justify-content: space-between;
   align-items: center;
-  padding-top: 50px;
+  padding-top: 40px;
 `;
 
 const ButtonBox = styled.div`
@@ -196,6 +269,66 @@ const BuyButtonBox = styled.div`
     font-weight: 500;
     color: white;
   }
+`;
+
+const FilterBox = styled.div`
+  display: flex;
+  justify-content: center;
+  padding: 100px 0;
+  div {
+    height: 30px;
+    border-bottom: 1px solid #949494;
+    span {
+      font-size: 20px;
+      font-weight: 500;
+      padding: 0 60px;
+      color: #949494;
+    }
+  }
+`;
+
+const ReviewBox = styled.div`
+  margin: 0 150px 100px;
+  h1 {
+    font-size: 25px;
+    font-weight: 600;
+    padding-bottom: 40px;
+    border-bottom: 1px solid #949494;
+  }
+`;
+
+const ReviewContent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 40px 100px;
+  border-bottom: 1px solid #949494;
+`;
+
+const ReviewLeftBox = styled.div`
+  width: 80%;
+  div {
+    display: flex;
+    align-items: center;
+    padding-bottom: 20px;
+    span {
+      padding-right: 10px;
+      font-size: 22px;
+    }
+    p {
+      font-size: 15px;
+      color: #949494;
+    }
+  }
+`;
+
+const ReviewText = styled.span`
+  font-size: 17px;
+  line-height: 25px;
+  color: #535353;
+`;
+
+const ReviewRightBox = styled.div`
+  /* padding: 0 100px; */
 `;
 
 export default ProductDetail;
